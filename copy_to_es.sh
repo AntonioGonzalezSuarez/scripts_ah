9e818631c2ac8ce7a96bc4b5e0c0def93219d26e
AntonioG <antonio.gonzalez@baobabsoluciones.es>
2023-01-25 12:20:28 +0100

    .

--- copy_to_es.sh
+++ copy_to_es.sh
@@ -29,8 +29,7 @@ EOF
 function update_es()
 {
 url="http://${1}:9200"
-curl -XPOST localhost:9210/_reindex?pretty -H 'Content-Type: application/json' -d \
-"$($generate_post_data() $url)"
+curl -XPOST localhost:9210/_reindex?pretty -H 'Content-Type: application/json' -d '$($generate_post_data() $url)'
 }
 
 update_es "worker01" || update_es "worker02"
