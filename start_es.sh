89343a0369b59d37f3603fcd79b754c8f7a4a9ca
AntonioG <antonio.gonzalez@baobabsoluciones.es>
2023-01-25 13:42:23 +0100

    .

--- start_es.sh
+++ start_es.sh
@@ -1,2 +1,2 @@
-export ES_JAVA_OPTS="$ES_JAVA_OPTS -Djava.io.tmpdir=/path/to/temp/dir"
+export ES_JAVA_OPTS="$ES_JAVA_OPTS -Djava.io.tmpdir=/home/alumnos/ah10/tmp"
 nohup /home/alumnos//ah10/elasticsearch-7.16.1/bin/elasticsearch
