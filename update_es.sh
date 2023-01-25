624fda4a374bf2b3ccc89560286e69d318c4ee26
AntonioG <antonio.gonzalez@baobabsoluciones.es>
2023-01-25 12:37:10 +0100

    .

--- update_es.sh
+++ update_es.sh
@@ -1,1 +1,9 @@
-./remove_index.sh && ./copy_to_es.sh
+function update_es()
+{
+    while True
+    do
+        ./remove_index.sh && ./copy_to_es.sh
+        sleep 2m
+    done
+}
+nohup update_es > /dev/null &
