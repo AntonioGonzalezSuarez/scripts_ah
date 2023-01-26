e09fc61d4a4552b12aa9b282ad6a0d2b12a6a398
alvaroarre <20191469@alu.comillas.edu>
2023-01-26 23:36:35 +0100

    cositas

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-export ES_JAVA_OPTS="$ES_JAVA_OPTS -Djava.io.tmpdir=/home/alumnos/ah10/tmp"
-nohup /home/alumnos//ah10/elasticsearch-7.16.1/bin/elasticsearch > /home/alumnos/ah10/logs/es.log 2>&1 & nohup /home/alumnos//ah10/kibana-7.16.2-linux-x86_64/bin/kibana > /dev/null 2>&1 &
+export ES_JAVA_OPTS="$ES_JAVA_OPTS -Djava.io.tmpdir=$HOME/tmp"
+nohup $HOME/elasticsearch-7.16.1/bin/elasticsearch > $HOME/logs/es.log 2>&1 & nohup $HOME/kibana-7.16.2-linux-x86_64/bin/kibana > /dev/null 2>&1 &
